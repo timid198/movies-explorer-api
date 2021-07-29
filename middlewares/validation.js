49d459afaed3f57e10e84ae085fd33c6edb4037a
Andrey Azovkin <adm.az-an@yandex.ru>
2021-07-29 16:46:16 +0300

    diplom back #1-18

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -75,7 +75,7 @@ const validateMovieCreateBody = celebrate({
     director: Joi.string().required().messages({ 'string.required': 'Поле "director" должно быть заполнено' }),
     duration: Joi.number().required().messages({
       'number.required': 'Поле "duration" должно быть заполнено',
-      'number.number': 'Поле "duration" должно быть числом.',
+      'number.base': 'Поле "duration" должно быть числом.',
     }),
     year: Joi.string().required().messages({ 'string.required': 'Поле "year" должно быть заполнено' }),
     description: Joi.string().required().messages({ 'string.required': 'Поле "description" должно быть заполнено' }),
