d82fdd73a8b3197604ee91bd4115ad722e752822
Andrey Azovkin <adm.az-an@yandex.ru>
2021-07-29 16:34:24 +0300

    diplom back #1-14

--- errors/celebrate-error.js
+++ errors/celebrate-error.js
@@ -4,7 +4,8 @@ const BadRequestError = require('./bad-request-err');
 const celebrateError = (err, req, res, next) => {
   console.log({ 1: err });
   if (isCelebrateError(err)) {
-    console.log({ 2: err.details });
+    console.log({ 2: err.details.get('params') });
+    console.log({ 2: err.details.get('body') });
     throw new BadRequestError(err.details.get('params').message);
   }
   next(err);
