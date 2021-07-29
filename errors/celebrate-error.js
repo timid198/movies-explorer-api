c17f179af1ff54017cd7e072e1700eb3fe2c38ec
Andrey Azovkin <adm.az-an@yandex.ru>
2021-07-29 16:38:50 +0300

    diplom back #1-16

--- errors/celebrate-error.js
+++ errors/celebrate-error.js
@@ -5,8 +5,13 @@ const celebrateError = (err, req, res, next) => {
   console.log({ 1: err });
   if (isCelebrateError(err)) {
     console.log({ 2: err.details.get('params') });
-    console.log({ 2: err.details.get('body') });
-    throw new BadRequestError(err.details.get('params').message);
+    console.log({ 3: err.details.get('body') });
+    if (err.details.get('params')) {
+      throw new BadRequestError(err.details.get('params').message);
+    }
+    if (err.details.get('body')) {
+      throw new BadRequestError(err.details.get('body').message);
+    }
   }
   next(err);
 };
