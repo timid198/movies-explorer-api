542f56208ba49a64f6b107071cdcbb3cb46f151f
Andrey Azovkin <adm.az-an@yandex.ru>
2021-08-29 14:51:53 +0300

    diplom back #1-32

--- middlewares/allowedCors.js
+++ middlewares/allowedCors.js
@@ -5,13 +5,13 @@ const allowedCors = [
   'http://localhost:3001',
 ];
 
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
 
-module.exports = (req, res, next) => {
-  console.log(req);
+module.exports = (req, res, next) => {  
   const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
+  console.log(requestHeaders);
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
