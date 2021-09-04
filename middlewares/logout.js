183ed8166dad7652cfd9dff40a11a968b7ddee10
Andrey Azovkin <adm.az-an@yandex.ru>
2021-09-04 23:59:47 +0300

    diplom back #1-78

--- middlewares/logout.js
+++ middlewares/logout.js
@@ -1,4 +1,4 @@
 module.exports = (req, res) => {
   res.clearCookie('jwt', { httpOnly: true, sameSite: 'none', secure: true });
-  return res.status(200).send();
+  return res.status(200).redirect('/');
 };
